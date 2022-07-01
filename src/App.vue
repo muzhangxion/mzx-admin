a021663ecc01e1bf8f5f7efab255f3f6da6c8f69
muzhanggege <254497987@qq.com>
2022-07-01 22:27:44 +0800

    删除首页img

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <div style="display: flex">
       <BaseSide />
       <div>
-        <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
         <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
       </div>
     </div>
